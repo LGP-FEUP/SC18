7a718f9c1ffa29b835453d0b3784a32f6453a7d9
Ana Barros <up201806593@fe.up.pt>
2022-05-07 23:08:57 +0100

    Addded: default faculty as FEUP

--- mobile/lib/models/user.dart
+++ mobile/lib/models/user.dart
@@ -1,14 +1,16 @@
 class UserModel {
-  final String email, password, fName, lName, faculty;
+  final String email, password, fName, lName, faculty_origin, erasmus_faculty;
   String uid = "";
 
-  UserModel(this.email, this.password, this.fName, this.lName, this.faculty);
+  UserModel(this.email, this.password, this.fName, this.lName,
+      this.faculty_origin, this.erasmus_faculty);
 
-  Map<String,String> getInfo() {
+  Map<String, String> getInfo() {
     return {
       "firstName": fName,
       "lastName": lName,
-      "faculty_origin_id": faculty
+      "faculty_origin_id": faculty_origin,
+      "faculty_arriving_id": erasmus_faculty
     };
   }
 }
