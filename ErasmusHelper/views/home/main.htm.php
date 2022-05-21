b6dbb453b5e4d2faa5bad4ac3f1570264afe8e20
Ruben Almeida <almeidaruben35@gmail.com>
2022-05-21 21:18:58 +0100

    Beginning of the homepage

--- ErasmusHelper/views/home/main.htm.php
+++ ErasmusHelper/views/home/main.htm.php
@@ -1,2 +1,92 @@
-<?php
-echo "TODO";
+<div class="container-fluid px-5">
+    <div class="row justify-content-center">
+        <div class="col-auto text-center">
+            <h4>About SumsEra</h4>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-6 text-center">
+            <img src="https://cdn-images.rtp.pt/icm/noticias/images/b0/b0513996eff4b6bcb30cd81374a80d3f?w=860&q=90&rect=0,9,1280,702"
+                 class="rounded-img" alt="">
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-auto text-center">
+            <h2>To help integration and to provide a stress-free experience for ERASMUS students.</h2>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-7 text-center">
+            <h6 class="pb-0 mb-2">A company born at FEUP.</h6>
+            <h6 class="pt-0"> A success story idealized in the LGP challenge</h6>
+        </div>
+    </div>
+    <div id="row-with-video" class="row mt-5 px-xl-5">
+        <div class="col background-white ultra-small-rounded py-5">
+            <div class="row h-100">
+                <div class="col-6 border-divisor-right">
+                    <h3>About Us</h3>
+                    <p>A group of 10 students from LGP Course</p>
+                </div>
+                <div class="col-6">
+                    <iframe width="100%" height="100%" src="https://www.youtube.com/embed/PRACK9_A5AE?autoplay=1&mute=1"
+                            title="YouTube video player" frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row mt-5 mb-3">
+        <h3>Erasmus in Figures</h3>
+    </div>
+    <div class="row px-xl-5 justify-content-around">
+        <div class="col-4 col-xl-2 mr-3 mb-3 text-center homepage-figure background-white small-rounded">
+            <h3 class="py-3">34 Countries</h3>
+            <p>Inside and outside European Union</p>
+        </div>
+        <div class="col-4 col-xl-2 mb-3 text-center homepage-figure background-white small-rounded">
+            <h3 class="py-3">4M</h3>
+            <p>The number of Students that benefited from Erasmus Program </p>
+        </div>
+        <div class="col-4 col-xl-2 mb-3 text-center homepage-figure background-white small-rounded">
+            <h3 class="py-3">+300K</h3>
+            <p>The number of Students that sign for Erasmus yearly </p>
+        </div>
+        <div class="col-4 col-xl-2 mb-3 text-center homepage-figure background-white small-rounded">
+            <h3 class="py-3">31K</h3>
+            <p>Spain is the #1 country for Erasmus. It receives 31 thousand students yearly</p>
+        </div>
+    </div>
+    <div id="row-with-video" class="row mt-5 px-xl-5">
+        <div class="col background-white ultra-small-rounded py-5">
+            <div class="row h-100">
+                <div class="col-6 border-divisor-right">
+                    <h3>What is Erasmus Helper</h3>
+                    <p>The product we present you is Erasmus Helper</p>
+                </div>
+                <div class="col-6">
+                    <iframe width="100%" height="100%" src="https://www.youtube.com/embed/1FJHYqE0RDg?autoplay=0&mute=1"
+                            title="YouTube video player" frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="row-with-video" class="row mt-5 px-xl-5">
+        <div class="col background-white ultra-small-rounded py-5">
+            <div class="row h-100">
+                <div class="col-6 border-divisor-right">
+                    <h3>FEUP & LGP</h3>
+                    <p>The product we present you is Erasmus Helper</p>
+                </div>
+                <div class="col-6">
+                    <iframe width="100%" height="100%" src="https://www.youtube.com/embed/akObFAN9sgw?autoplay=0&mute=1"
+                            title="YouTube video player" frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    ></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
