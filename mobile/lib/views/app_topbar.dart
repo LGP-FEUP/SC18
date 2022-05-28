055afdd456d379b9b06f636f782671a583df99f1
Ana Barros <up201806593@fe.up.pt>
2022-05-28 18:07:53 +0100

    Add: floating button

--- mobile/lib/views/app_topbar.dart
+++ mobile/lib/views/app_topbar.dart
@@ -10,12 +10,14 @@ class AppTopBar extends StatelessWidget {
   final PreferredSizeWidget? bottom;
   final String title;
   final bool activateBackButton;
-  const AppTopBar(
-      {Key? key,
-      required this.body,
-      this.title = "Erasmus Helper",
-      this.bottom,
-      this.activateBackButton = false})
+  final FloatingActionButton? floatingButton;
+
+  const AppTopBar({Key? key,
+    required this.body,
+    this.title = "Erasmus Helper",
+    this.bottom,
+    this.floatingButton,
+    this.activateBackButton = false})
       : super(key: key);
 
   @override
@@ -26,26 +28,32 @@ class AppTopBar extends StatelessWidget {
     }
     return SafeArea(
         child: Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: true,
-        foregroundColor: Theme.of(context).primaryColor,
-        backgroundColor: Colors.white,
-        title: Text(title),
-        bottom: bottom,
-        actions: [
-          // TODO : change with avatar of the account
-          GestureDetector(
-              child: const CircleAvatar(
-                backgroundImage: AssetImage("assets/avatar.jpg"),
-              ),
-              onTap: () => Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const ProfileScreen()))),
-          // TODO : add notification system
-          IconButton(onPressed: () {}, icon: const Icon(Icons.notifications))
-        ],
-      ),
-      drawer: drawer,
-      body: body,
-    ));
+          floatingActionButton: floatingButton,
+          appBar: AppBar(
+            automaticallyImplyLeading: true,
+            foregroundColor: Theme
+                .of(context)
+                .primaryColor,
+            backgroundColor: Colors.white,
+            title: Text(title),
+            bottom: bottom,
+            actions: [
+              // TODO : change with avatar of the account
+              GestureDetector(
+                  child: const CircleAvatar(
+                    backgroundImage: AssetImage("assets/avatar.jpg"),
+                  ),
+                  onTap: () =>
+                      Navigator.push(context,
+                          MaterialPageRoute(
+                              builder: (context) => const ProfileScreen()))),
+              // TODO : add notification system
+              IconButton(
+                  onPressed: () {}, icon: const Icon(Icons.notifications))
+            ],
+          ),
+          drawer: drawer,
+          body: body,
+        ));
   }
 }
