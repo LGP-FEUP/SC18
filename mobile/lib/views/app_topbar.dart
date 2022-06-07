7a2952856326366703c9c343394c6cc40bfcf09a
antoniog <antonio.gomes@etu.univ-tours.fr>
2022-06-07 14:45:18 +0100

    fix: bug of the profile button fixed

--- mobile/lib/views/app_topbar.dart
+++ mobile/lib/views/app_topbar.dart
@@ -10,16 +10,18 @@ class AppTopBar extends StatelessWidget {
   final PreferredSizeWidget? bottom;
   final String title;
   final bool activateBackButton;
+  final bool isOnProfilePage;
   final FloatingActionButton? floatingButton;
 
   const AppTopBar(
       {Key? key,
-      required this.body,
-      this.title = "Erasmus Helper",
-      this.bottom,
-      this.floatingButton,
-      this.activateBackButton = false})
-      : super(key: key);
+        required this.body,
+        this.title = "Erasmus Helper",
+        this.bottom,
+        this.floatingButton,
+        this.activateBackButton = false,
+        this.isOnProfilePage = false
+      }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -29,32 +31,36 @@ class AppTopBar extends StatelessWidget {
     }
     return SafeArea(
         child: Scaffold(
-      floatingActionButton: floatingButton,
-      appBar: AppBar(
-        automaticallyImplyLeading: true,
-        foregroundColor: Theme.of(context).primaryColor,
-        backgroundColor: Colors.white,
-        title: Text(
-          title,
-          style: const TextStyle(fontSize: 23, fontWeight: FontWeight.w600),
-        ),
-        bottom: bottom,
-        actions: [
-          // TODO : change with avatar of the account
-          GestureDetector(
-              child: const CircleAvatar(
-                backgroundImage: AssetImage("assets/avatar.png"),
-              ),
-              onTap: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const ProfileScreen()))),
-          // TODO : add notification system
-          IconButton(onPressed: () {}, icon: const Icon(Icons.notifications))
-        ],
-      ),
-      drawer: drawer,
-      body: body,
+          floatingActionButton: floatingButton,
+          appBar: AppBar(
+            automaticallyImplyLeading: true,
+            foregroundColor: Theme.of(context).primaryColor,
+            backgroundColor: Colors.white,
+            title: Text(
+              title,
+              style: const TextStyle(fontSize: 23, fontWeight: FontWeight.w600),
+            ),
+            bottom: bottom,
+            actions: [
+              // TODO : change with avatar of the account
+              GestureDetector(
+                  child: const CircleAvatar(
+                    backgroundImage: AssetImage("assets/avatar.png"),
+                  ),
+                  onTap: () {
+                    if (!isOnProfilePage) {
+                      Navigator.push(context, MaterialPageRoute(
+                          builder: (context) => const ProfileScreen()
+                      ));
+                    }
+                  }
+                ),
+              // TODO : add notification system
+              IconButton(onPressed: () {}, icon: const Icon(Icons.notifications))
+            ],
+          ),
+          drawer: drawer,
+          body: body,
     ));
   }
 }
